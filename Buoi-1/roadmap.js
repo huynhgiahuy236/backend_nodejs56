ff8943a7fa84e5a5c2ee348256c726430b75bc44
huynhgiahuy <huynhgiahuy236@gmail.com>
2026-07-18 17:08:15 +0700

    first commit

---
+++ Buoi-1/roadmap.js
@@ -0,0 +1,41 @@
+const roadMap = {
+    Database: {
+        "Buổi 1": "Khai giảng - cài đặt",
+        "Buổi 2": "MySQL, Basic CRUD database, table",
+        "Buổi 3": "Query, relationship",
+    },
+
+    Express: {
+        "Buổi 4": "khởi tạo source, áp dụng mô hình mvc",
+        "Buổi 5": "middleware, áp dụng middleware",
+        "Buổi 6": "Code first, data first, orm, prisma",
+        "Buổi 7": "Phân trang, filter, ráp với FE",
+    },
+
+    Auth: {
+        "Buổi 8": "Xây dựng đăng ký đăng nhập, accesstoken, jwt",
+        "Buổi 9": "build middleware bảo vệ API",
+        "Buổi 10":
+            "refreshtoken - đăng nhập bằng gmail, authentication bằng google",
+        "Buổi 11": "upload file local, upload vào cloud",
+        "Buổi 12": "swagger api",
+    },
+
+    Nestjs: {
+        "Buổi 13": "socket.io, xây dựng chức năng chat 1-1, chat nhóm",
+        "Buổi 14": "Nestjs, tìm hiểu cấu trúc và cách sử dụng",
+        "Buổi 15": "Microservice, rabbitmq - gửi mail, tách microservce",
+        "Buổi 16": "Cache, cache redis",
+        "Buổi 17": "Elasticsearch, kibana",
+    },
+
+    Docker: {
+        "Buổi 18": "tìm hiểu docker, dockerfile, docker compose",
+        "Buổi 19": "aws ec2 - map domain cloudfare, cicd",
+    },
+};
+
+function sum(thamSoA, thamSoB) { }
+
+sum(1, 2);
+

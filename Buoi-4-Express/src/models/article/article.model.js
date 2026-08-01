48e82a280c68e039c554f220dd4872b18c4f1a5a
huynhgiahuy <huynhgiahuy236@gmail.com>
2026-08-01 14:41:48 +0700

    Refactor article model definition and improve structure

--- Buoi-4-Express/src/models/article/article.model.js
+++ Buoi-4-Express/src/models/article/article.model.js
@@ -1,23 +1,72 @@
 import { DataTypes } from "sequelize";
-import sequelize from "../../common/sequelize/connect.sequelize.js";
-import { table } from "node:console";
+import sequelize from "../common/sequelize/connect.sequelize.js";
 
-const articleModel = sequelize.define(
-    'Article', {
+const Article = sequelize.define(
+  "Article", //tên cục bộ sẽ sử dụng trong code/service
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     title: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-},
-    {
-        tableName: "Articles"
-    }
-
+    Content: {
+      type: DataTypes.TEXT,
+      // allowNull: true, giá trị mặc định là true nên không cần khai báo
+    },
+    imageURL: {
+      type: DataTypes.STRING,
+    },
+    views: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        key: "id", // khóa chính của bảng tham chiếu
+        model: "Users", // tên bảng tham chiếu trong db
+      },
+    },
+    deletedBy: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    isDeleted: {
+      type: DataTypes.BOOLEAN, // chỉ bao gồm 2 giá trị là 0 và 1 tương ứng với false và true
+      allowNull: false,
+      defaultValue: 0,
+    },
+    deletedAt: {
+      type: "TIMESTAMP",
+      allowNull: true,
+      defaultValue: null,
+    },
+    createdAt: {
+      type: "TIMESTAMP",
+      allowNull: false,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updatedAt: {
+      type: "TIMESTAMP",
+      allowNull: false,
+      defaultValue: sequelize.literal(
+        "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+      ),
+    },
+  },
+  {
+    tableName: "Articles",
+    timestamps: false
+    // tắt tính năng tự đông tạo 2 cột createAt và updateAt
+  },
 );
-export default articleModel
+
+export default Article;

7fca7029fb7d0b63bf66acf89cd3e310d7d48eaf
huynhgiahuy <huynhgiahuy236@gmail.com>
2026-08-09 16:15:46 +0700

    fix(auth): add user existence check in protect middleware and improve access token validation

--- Buoi-4-Express/src/common/middlewares/protect.middleware.js
+++ Buoi-4-Express/src/common/middlewares/protect.middleware.js
@@ -1,12 +1,22 @@
 import { tokenService } from "../../services/token.service.js"
 import { BadRequestException } from "../helpers/exception.helper.js"
+import { prisma } from "../prisma/connect.prisma.js"
 
 export const protect = (req, res, next) => {
-    const {accessToken} = req.cookies
-    if(!accessToken) {
+    const { accessToken } = req.cookies
+    if (!accessToken) {
         throw new BadRequestException("Không có AccessToken")
     }
     // verify accessToken
     const decode = tokenService.verifyAccessToken(accessToken);
+    const userExits = await prisma.users.findUnique({
+        where: {
+            id: decode.userId
+        }
+    })
+    if (!userExits) {
+        throw new BadRequestException("Người dùng không tồn tại")
+    }
+    req.user = userExits
     next()
 }

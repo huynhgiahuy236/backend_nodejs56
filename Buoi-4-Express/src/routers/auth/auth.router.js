a4ccd55c19886c371060b915f3e05ce1fa2a11fe
huynhgiahuy <huynhgiahuy236@gmail.com>
2026-08-15 17:07:27 +0700

    feat(auth): add Google OAuth login route with passport authentication

--- Buoi-4-Express/src/routers/auth/auth.router.js
+++ Buoi-4-Express/src/routers/auth/auth.router.js
@@ -3,6 +3,7 @@ import { authController } from '../../controllers/auth.controller.js';
 import { protect } from '../../common/middlewares/protect.middleware.js';
 import { protectv2 } from '../../common/middlewares/protectv2.middleware.js';
 import { limitLogin } from '../../common/middlewares/rateLimit.middleware.js';
+import passport from 'passport';
 
 const authRouter = express.Router();
 
@@ -11,6 +12,8 @@ authRouter.post('/register', authController.register);
 authRouter.post('/login', limitLogin, authController.login);
 authRouter.get("/get-info", protect, authController.getInfo)
 authRouter.post("/refresh-token", authController.refreshToken)
-authRouter.post("/auth/google")
+// khi user click vao login gg - api login gg se dc kich hoat
+// passport kich hoat va redirect user toi trang chon tai khoan gg, dong thoi gui scope ma minh da yeu cau
+authRouter.get("/google",passport.authenticate("google",{scope:["profile","email"]}))
 // authRouter.get("/get-info", protectv2, authController.getInfo)
 export default authRouter;

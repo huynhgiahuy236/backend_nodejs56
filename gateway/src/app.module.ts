b25b3f35a28052fb6e883b9ae09a7362e23a42a5
huynhgiahuy236 <huynhgiahuy236@gmail.com>
2026-08-29 15:35:57 +0700

    feat(prisma): integrate Prisma module and implement database connection validation

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { AuthModule } from './module-api/auth/auth.module.js';
+import { PrismaModule } from './module-system/prisma/prisma.module.js';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- gateway/src/module-system/prisma/prisma.service.ts
+++ gateway/src/module-system/prisma/prisma.service.ts
@@ -1,30 +1,30 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaClient } from "./generated/prisma/client.js";
+import { Injectable, type OnModuleInit } from "@nestjs/common";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { DATABASE_URL } from "../../common/constant/app.constant.js";
+import { PrismaClient } from "./generated/prisma/client.js";
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const url = new URL(DATABASE_URL as string)
-    console.log(url)
-
+    const url = new URL(DATABASE_URL);
     const adapter = new PrismaMariaDb({
-      user:url.username,
+      user: url.username,
       password: url.password,
-      host:url.pathname,
+      host: url.hostname,
       port: Number(url.port),
-      database: url.pathname.slice(1)
+      database: url.pathname.slice(1),
     });
+
     super({ adapter });
   }
-   async onModuleInit(){
-    //kiểm tra kết nối
+
+  async onModuleInit(): Promise<void> {
+    // Kiểm tra kết nối cơ sở dữ liệu khi Nest khởi tạo module.
     try {
       await this.$queryRaw`SELECT 1 + 1 AS result`;
-      console.log("✅ [PRISMA] Connection has been established successfully.");
+      console.log("[PRISMA] Connection has been established successfully.");
     } catch (error) {
-      console.error("❌ [PRISMA] Unable to connect to the database:", error);
+      console.error("[PRISMA] Unable to connect to the database:", error);
     }
   }
 }
